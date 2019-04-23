9811a09e22a6eb8238f540ccc80938f657797cd4
Bmord01 <bdrmord001@gmail.com>
2019-04-23 02:41:17 -0500

    changes

--- FinalProj.py
+++ FinalProj.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 import numpy as np
 
+qd.ApiConfig.api_key = "ix7crpf3zvtxR66RyR55"
+
 ATT = qd.get("WIKI/T",trim_start = "2000-1-1", trim_end="2018-1-1",collumn_index=1)
 VERIZON = qd.get("WIKI/VZ",trim_start = "2000-1-1", trim_end="2018-1-1",collumn_index=1)
 SPRINT = qd.get("WIKI/S",trim_start = "2000-1-1", trim_end="2018-1-1",collumn_index=1)
@@ -13,8 +15,8 @@ print(VERISON)
 print(SPRINT)
 '''
 plt.plot(ATT.index,ATT['Open'],label="AT&T")
-plt.plot(VERIZON.index,VERIZON['Open'],label="Verizon")
-plt.plot(SPRINT.index,SPRINT['Open'],label = "Sprint")
+plt.plot(VERIZON.index,VERIZON['Open'],'m',label="Verizon")
+plt.plot(SPRINT.index,SPRINT['Open'],'g',label = "Sprint")
 plt.title('Opening Stock Price for Phone Companies')
 plt.xlabel("Date")
 plt.ylabel("Open Price")
@@ -36,14 +38,44 @@ minV = VERIZON["Open"].min()
 minS = SPRINT["Open"].min()
 print(minA,minV,minS)
 for item in range(1,4527):
-    if(ATT[item]["Open"]==minA):
+    if(ATT["Open"][item]==minA):
         minADate = int(item)
-print(minADate)
+    if(VERIZON["Open"][item]==minV):
+        minVDate = int(item)
+    if(SPRINT["Open"][item]==minS):
+        minSDate = int(item)
+print(minADate,minVDate,minSDate)
+#print(ATT.iloc[minADate])
+#print(VERIZON.iloc[minVDate])
+#print(SPRINT.iloc[minSDate])
+yALow = [ATT.iloc[0]["Open"],ATT.iloc[minADate]["Open"]]
+yVLow = [VERIZON.iloc[0]["Open"],VERIZON.iloc[minVDate]["Open"]]
+ySLow = [SPRINT.iloc[0]["Open"],SPRINT.iloc[minSDate]["Open"]]
 
-plt.plot(xA[0:2],yA[0:2]);
-plt.plot(xA[0:2],yV[0:2]);
-plt.plot(xA[0:2],yS[0:2]);
-plt.savefig("TRENT.png")
+yAAfter = [ATT.iloc[minADate]["Open"],ATT.iloc[4526]["Open"]]
+yVAfter = [VERIZON.iloc[minVDate]["Open"],VERIZON.iloc[4526]["Open"]]
+ySAfter = [SPRINT.iloc[minSDate]["Open"],SPRINT.iloc[4526]["Open"]]
+
+xATTLow = [0,minADate]
+xVLow = [0,minVDate]
+xSLow = [0,minSDate]
+
+xATTAfter = [minADate,4526]
+xVAfter = [minVDate,4526]
+xSAfter = [ minSDate,4526]
+
+plt.plot(xA[0:2],yA[0:2])
+plt.plot(xA[0:2],yV[0:2])
+plt.plot(xA[0:2],yS[0:2])
+plt.savefig("OverallTrend.png")
+plt.clf()
+plt.plot(xATTLow[0:2],yALow[0:2],'b')
+plt.plot(xATTAfter[0:2],yAAfter[:2],'b')
+plt.plot(xVLow[:2],yVLow[:2],'m')
+plt.plot(xVAfter[:2],yVAfter[:2],'m')
+plt.plot(xSLow[:2],ySLow[:2],'g')
+plt.plot(xSAfter[:2],ySAfter[:2],'g')
+plt.savefig("TREND.png")
 
 
 
